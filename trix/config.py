a423eba60c1853ef8c7273f3f780c8559394f2b4
Darcy Laycock <sutto@sutto.net>
2013-05-04 18:20:24 +0800

    Add in config, runner tweaks.

---
+++ trix/config.py
@@ -0,0 +1,19 @@
+# Trix configuration / management.
+
+class Configuration:
+
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+    def merge(self, object):
+        self.__dict__.update(object.__dict__)
+
+    def load_file(self, path):
+      pass
+
+    def __repr__(self):
+      data = self.__dict__
+      information = " ".join(["%s=%r" % (k, data[k]) for k in data])
+      return "<trix.config.Configuration %s>" % information
+
+defaults = Configuration(width=11, buffer=1, input_file=None, output_file=None)
--- trix/runner.py
+++ trix/runner.py
@@ -1,2 +1,15 @@
+import argparse
+from trix.config import defaults, Configuration
+
+parser = argparse.ArgumentParser(description="Trix configuration information")
+parser.add_argument('-c', '--config', dest='config_file', help='A configuration file to read details from', nargs='?')
+parser.add_argument('-w', '--width', dest='width', help='The width of the tetrist board', default=defaults.width, type=int)
+parser.add_argument('-b', '--buffer', dest='buffer', help='The size of the buffer', default=defaults.buffer, type=int)
+parser.add_argument('input_file', help='The file to read input from')
+parser.add_argument('output_file', help='The file to write output to')
+
 def main(argv):
-  print "COMMAND:", repr(argv)
+    parsed_arguments = parser.parse_args(argv[1:])
+    configuration = Configuration()
+    configuration.merge(parsed_arguments)
+    print repr(configuration)
