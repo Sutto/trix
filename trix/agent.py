b5de69515dbe6528e78bc6a2c909df7543b89208
Darcy Laycock <sutto@sutto.net>
2013-05-19 16:38:46 +0800

    Add in a first iteration of an itnelligent agent.

--- trix/agent.py
+++ trix/agent.py
@@ -1,4 +1,5 @@
 from .actions import *
+from .utilities import Variation
 import random
 import sys
 
@@ -38,8 +39,87 @@ class RandomAgent(Agent):
     possible_offsets = self.environment.possible_left_offsets_for(rotation)
     return PlaceNextPiece(rotation, random.choice(possible_offsets))
 
+class PrimaryAgent(Agent):
+
+  # We keep track of the maximum depth (e.g. 5 forks), and we disallow too horrible utilities, based on a historic amount.
+  # Note that we will only disallow if they are increasing / there is no improvement for a given utility. THe idea here is
+  # that doing so probably won't improve it substantially.
+  #
+  # The design of this agent is such:
+  # Given a starting environment, to find the best possible action - start with the initial action by generating a list of all actions.
+  # Then, do the equivalent of an interative-deepening search, so scanning across and then down, killing it if any of the utilities are
+  # unpromising.
+  #
+  # So, we're searching for the most 'promising' version of this. We use an ordered scoreboard object for this (implemented as a binary tree)
+  # and keep track of running details about the utility. We kill searches on a node when it's either hit an acceptable maximum (we need a way)
+  # to calculate this.
+
+  def choose_action(self, percept):
+    print("Choose_action", percept.piece)
+    root_environment    = self.environment
+    root_variation      = Variation(root_environment, -1, [])
+    expanded_variations = self.expand_variations([root_variation])
+    return self.best_variation_from(expanded_variations)
+
+  def expand_variations(self, variations):
+    to_expand = list(variations)
+    print(to_expand)
+    while to_expand:
+      candidate = to_expand.pop(0)
+      if self.should_explore_variation(candidate):
+        # We look at the alternative, but don't yield ourself. We only care about the best child.
+        added = False
+        for child in self.child_variations_for(candidate):
+          to_expand.append(child)
+          added = True
+        if not added: yield candidate
+      else:
+        yield candidate
+
+  def should_explore_variation(self, variation):
+    # Hard code to a look ahead of 1 for the moment.
+    return variation.depth < 3
+
+  def child_variations_for(self, variation):
+    variations  = []
+    environment = variation.environment
+    percept     = environment.perceive()
+    if percept:
+      for action in self.possible_actions_for(environment, percept.piece):
+        variations.append(variation.fork(action))
+    return variations
+
+  def best_variation_from(self, variations):
+    best_utility   = float("inf")
+    best_variation = None
+    for variation in variations:
+      utility = variation.utility
+      if utility  < best_utility:
+        best_utility = utility
+        best_variation = variation
+    if best_variation:
+      return best_variation.root_action
+    else:
+      return None
+
+
+  def possible_actions_for_piece(self, env, piece, klass):
+    # TODO: Use precomputed rotations.
+    for r in piece.rotations():
+      for i in env.possible_left_offsets_for(r):
+        yield klass(r, i)
+
+  def possible_actions_for(self, env, piece):
+    if not env.buffer_is_full(): yield AddToBuffer(piece)
+    for action in self.possible_actions_for_piece(env, piece, PlaceNextPiece): yield action
+    # for buffered_piece in env.buffer:
+    #   for action in self.possible_actions_for_piece(env, buffered_piece, PlaceFromBuffer): yield action
+
+
+
 Agents = {
-  'default': RandomAgent,
+  'default': PrimaryAgent,
+  'primary': PrimaryAgent,
   'random':  RandomAgent
 }
 ValidAgents = Agents.keys()
---
+++ trix/utilities.py
@@ -0,0 +1,60 @@
+class Referee(object):
+  """
+  Given a board object, gives an approximate score representing the quality of the given
+  board. Note that a lower score is better, and to that end we use positive and negative
+  weighting.
+  """
+
+  @classmethod
+  def calculate(klass, board):
+    return klass(board).score()
+
+  __slots__ = ['board', 'environment']
+
+  weights = {
+    'valleys':       -0.5,
+    'holes':          2.0,
+    'cleared':       -2.5,
+    'maximum_height': 2.0
+  }
+
+  def __init__(self, board):
+    self.board = board
+
+  def score(self):
+    return sum(self.factor(name) for name in self.weights.keys())
+
+  def factor(self, name):
+    return self.weights[name] * getattr(self, name)()
+
+  def cleared(self):        return self.board.cleared
+  def maximum_height(self): return self.board.maximum_height
+
+  def holes(self):  return 0
+  def valleys(self): return 0
+
+class CutoffMetric(object):
+
+  def should_cutoff(self, depth, environment): return True
+
+class Variation(object):
+
+  __slots__ = ['environment', 'depth', 'actions', '_utility']
+
+  def __init__(self, environment, depth, actions):
+    self.environment = environment
+    self.depth       = depth
+    self.actions     = actions
+    self._utility    = None
+
+  def fork(self, action):
+    return Variation(self.environment.fork(action), self.depth + 1, self.actions + [action])
+
+  @property
+  def root_action(self): return self.actions[0]
+
+  @property
+  def utility(self):
+    if self._utility is None:
+      self._utility = Referee.calculate(self.environment.board)
+    return self._utility
